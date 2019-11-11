c349b2ed94c7eada3728c0be9f9559494a8602e2
codeasaglacier <remingtonshock@gmail.com>
2019-11-10 16:42:19 -0900

    set up api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState , useEffect }from "react";
 import Header from "./components/Header.js";
+import axios from 'axios';
 
 
-export default function App() {
+  const App = () => {
+
+  const [character, setCharacter] = useState([])
+  // console.log(`Character: ${character}`)
+
+  useEffect(() => {
+    axios
+      .get('https://rick-api.herokuapp.com/api/character/')
+      .then(response => {
+        console.log(response.data.results)
+        // setCharacter(response.data.results)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
   return (
     <main>
       <Header />
     </main>
   );
 }
+
+export default App
